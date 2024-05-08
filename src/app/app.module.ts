b038363515ec93107b877624f3e3e611a72bf01f
Tyson La <Tysonala@hotmail.com>
2024-05-08 12:06:26 -0500

    utpo mongoDB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule } from '@angular/common/http'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,6 +33,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserAnimationsModule,
     MatToolbarModule,
     MatExpansionModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
